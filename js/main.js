4da86b4d83f3dfbe1ffafa3c2ae75090390d4c8e
Randal Sean Harrison <rharriso@nd.edu>
2024-10-10 12:35:45 -0400

    updating the app with new interface elements

--- js/main.js
+++ js/main.js
@@ -13,6 +13,35 @@ $(function () {
       document.getElementById('latex-output').value = latexCode;
    });
 
+   // Reset button
+   $("#reset-form").on("click", function () {
+      // Open the dialog box onClick
+      $("#dialog-confirm").dialog("open");
+   });
+
+
+   // Reset form button
+   $("#dialog-confirm").dialog({
+      autoOpen: false,
+      resizable: false,
+      height: "auto",
+      width: 400,
+      modal: true,
+      buttons: {
+         "Cancel": function () {
+            $(this).dialog("close");
+         },
+         "Reset Page": function () {
+            // Turns the green checked circles to open empty circles
+            window.location.reload(true);
+            //
+            // // Stop propagation of the event
+            // event.stopPropagation();
+            // // Close the dialog
+            // $("#dialog-confirm").dialog("close");
+         }
+      }
+   });
 
 
 
