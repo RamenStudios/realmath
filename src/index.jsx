804e3c17e190f4a653713438004f3911e5c28a9d
RamenStudios <adasanrodriguez@gmail.com>
2025-08-25 15:11:28 -0400

    reformatted for vite

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,12 @@
 import { createRoot } from 'react-dom/client'
-import { App } from './components/App'
+import { StrictMode } from 'react'
+import App from './components/App'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
-import './stylesheets/main.css'
+import '../stylesheets/main.css'
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App/>);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
