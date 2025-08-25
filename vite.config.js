a1c1cc782f16535db9649fce885301e6d9d1f0c0
RamenStudios <adasanrodriguez@gmail.com>
2025-08-25 15:53:47 -0400

    Add repo name to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const HOST_URL = 'https://ramenstudios.github.io/realmath/'
+const REPO_NAME = '/realmath/'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `${HOST_URL}`,
+  base: `${REPO_NAME}`,
 })
