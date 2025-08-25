8320fada7dff1cc25a4effe4220e46bef03dc1f0
Adamari R <97457051+RamenStudios@users.noreply.github.com>
2025-08-25 15:42:00 -0400

    added base url to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const HOST_URL = 'https://ramenstudios.github.io/realmath/'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `${HOST_URL}`,
 })
