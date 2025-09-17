643f662ab1e441b52560cd69485036fff88188c2
RamenStudios <adasanrodriguez@gmail.com>
2025-09-17 14:25:30 -0400

    tweaking font sizes

--- src/components/about/dabout.jsx
+++ src/components/about/dabout.jsx
@@ -6,12 +6,10 @@ export const About = () =>
                 <div class="title">Visualize complex math formulas with RealMath™!</div>
             </div>
             <div class="row">
-                <div class="col-1"></div>
-                <div class="col">
-                    Add and define graph components below, then VISUALIZE to watch LaTeX equations come to life in augmented reality on your mobile device. 
-                    Boost your understanding and bring abstract math into the real world in seconds!
-                </div>
-                <div class="col-1"></div>
+                    <div class='text-center'>
+                        Add and define graph components below, then VISUALIZE to watch LaTeX equations come to life in augmented reality on your mobile device. 
+                        Boost your understanding and bring abstract math into the real world in seconds!
+                    </div>
             </div>
         </div>
     )
